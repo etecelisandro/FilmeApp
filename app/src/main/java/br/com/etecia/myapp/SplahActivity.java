f5db2935c1198cf7930d3468c3eca14cd46ce398
fran22etec <fran.etec22@gmail.com>
2023-05-11 21:17:24 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/SplahActivity.java
@@ -0,0 +1,24 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splah_layout);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            startActivity(new Intent(getApplicationContext(),
+                    LoginActivity.class));
+            finish();
+            }
+        },3000);
+    }
+}
